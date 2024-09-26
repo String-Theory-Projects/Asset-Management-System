0fc56ef45fcd5a2b668654dbb69977b1d4b61c37
najeeb-yusuf <yusufnajlawal@gmail.com>
2024-09-26 23:54:21 +0100

    add pagination helper

--- utils/helpers.py
+++ utils/helpers.py
@@ -2,6 +2,10 @@ from decimal import Decimal
 from django.core.cache import cache
 from django.core.serializers.json import DjangoJSONEncoder
 
+from rest_framework.pagination import PageNumberPagination
+
+
+# ----------- Data manipulation -------------
 def get_cached_data(cache_key, queryset):
     data = cache.get(cache_key)
     if not data:
@@ -14,3 +18,10 @@ class CustomJSONEncoder(DjangoJSONEncoder):
         if isinstance(obj, Decimal):
             return float(obj)
         return super().default(obj)
+
+# ----------- API helpers -------------
+
+class CustomPageNumberPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
