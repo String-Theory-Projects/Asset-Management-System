1b3c7efa06c3d661ac6325e56acc8ceab9026bcd
Bnabdulwasiu <abdulrahmanabdulwasiu111@gmail.com>
2024-09-29 12:48:53 +0100

    Asset number update

--- assets/serializers.py
+++ assets/serializers.py
@@ -3,13 +3,26 @@ from core.models import Asset
 from django.contrib.auth import get_user_model
 
 
-User = get_user_model()
-
 class AssetSerializer(serializers.ModelSerializer):
+    asset_number = serializers.SerializerMethodField()
+
     class Meta:
         model = Asset
         fields = "__all__"
-        read_only_fields = ['id']
+        read_only_fields = ['id', 'asset_number']
+
+# Genearating asset number
+    def get_asset_number(self, obj):
+        
+        user_id = obj.roles.first().user.id
+        # Get total count of user assets till this point
+        user_assets = Asset.objects.filter(roles__user__id=user_id).order_by('created_at')
+        # Getting the asset ordinal number from a list of assets
+        asset_number = list(user_assets).index(obj) + 1
+        formatted_user_id = f"{user_id:05}"
+        formatted_asset_number = f"{asset_number:03}"
+
+        return f"TP{formatted_user_id}{formatted_asset_number}"
 
     def create(self, validated_data):
         # If you have a many-to-many field, exclude it from the creation of the object
--- core/serializers.py
+++ core/serializers.py
@@ -1,16 +1,20 @@
 from rest_framework import serializers
 from .models import User
-from django.contrib.auth import get_user_model
+# from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 
-
-User = get_user_model()
+# User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    user_id = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', 'account_number', 'bank', 'avatar']
+        fields = ['user_id', 'first_name', 'last_name', 'email', 'account_number', 'bank', 'avatar']
+    
+    #function to get the user id
+    def get_user_id(self, obj):
+        return obj.pk
 
     def validate_email(self, value):
         if User.objects.filter(email=value).exists():
