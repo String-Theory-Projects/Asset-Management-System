e473fb7cdc91287d4e009e20685cc127b88bf090
Bnabdulwasiu <abdulrahmanabdulwasiu111@gmail.com>
2024-10-02 21:01:07 +0100

    Worked on imports

--- core/utils.py
+++ core/utils.py
@@ -1,10 +1,10 @@
 from django.db.models import Sum
-from .models import Payment
+from .models import Transaction
 
 def payment_aggregator(asset):
     
     # Calculating the total payments for a given asset
-    total_payments = Payment.objects.filter(asset_id=asset.id, payment_status='completed').aggregate(total=Sum('amount'))
+    total_payments = Transaction.objects.filter(asset_id=asset.id, payment_status='completed').aggregate(total=Sum('amount'))
     # If no payments set to 0
     total_amount = total_payments['total'] if total_payments['total'] is not None else 0
     asset.total_revenue = total_amount
