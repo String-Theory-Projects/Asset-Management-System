0b8c3c4b3a80d11501e1de2346515dd15b145c60
Yaqcodes <shuaibkhaleefa@gmail.com>
2024-10-11 17:22:19 +0100

    added process_transaction(), schedule_sub_asset_expiry() and VerifyPayment faulty

--- core/permissions.py
+++ core/permissions.py
@@ -1,5 +1,28 @@
-from rest_framework import permissions
+from rest_framework import permissions, authentication
 from core.models import Role
+from rest_framework import exceptions
+from django.contrib.auth import get_user_model
+import os
+
+
+User = get_user_model()
+
+class SystemUserAuthentication(authentication.BaseAuthentication):
+    def authenticate(self, request):
+        system_token = request.META.get('HTTP_X_SYSTEM_TOKEN')
+        if not system_token:
+            return None
+
+        if system_token != os.getenv('SECRET_KEY'):  # Replace with a secure token
+            raise exceptions.AuthenticationFailed('Invalid system token')
+
+        try:
+            user = User.objects.get(username='info@trykey.com')
+        except User.DoesNotExist:
+            raise exceptions.AuthenticationFailed('System user does not exist')
+
+        return (user, None)
+
 
 class IsAdmin(permissions.BasePermission):
     """
