edb1c7c8c70f16aaa9b8775fe46efd315ab5f088
Yaqcodes <shuaibkhaleefa@gmail.com>
2024-10-12 06:10:35 +0100

    set prod request url

--- hotel_demo/settings.py
+++ hotel_demo/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 import os
 
 
-# SYSTEM_USER_REQUEST_DOMAIN = 'https://dashboard.trykeyprotocol.com'
-SYSTEM_USER_REQUEST_DOMAIN = 'http://localhost:8000'
+SYSTEM_USER_REQUEST_DOMAIN = 'https://dashboard.trykeyprotocol.com'
+# SYSTEM_USER_REQUEST_DOMAIN = 'http://localhost:8000'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
