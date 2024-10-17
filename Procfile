43a7b99c9b70485e1b1a38dbdc06c6745905a58b
Yaqcodes <shuaibkhaleefa@gmail.com>
2024-10-17 22:26:56 +0100

    fixed celery Procfile command

--- Procfile
+++ Procfile
@@ -1,4 +1,3 @@
 web: gunicorn hotel_demo.wsgi --log-file - 
 worker: celery -A hotel_demo worker --loglevel=info
-worker: celery -A hotel-demo worker --loglevel=info
 beat: celery -A hotel_demo beat --loglevel=info
--- hotel_demo/celery.py
+++ hotel_demo/celery.py
@@ -3,6 +3,6 @@ from celery import Celery
 from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotel_demo.settings')
 
-app = Celery('hotel_demo', broker=settings.CELERY_BROKER_URL)
+app = Celery('hotel_demo', broker=settings.CELERY_BROKER_URL, backend=settings.CELERY_BROKER_URL)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
