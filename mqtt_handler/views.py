41e47b13f50bcb516529586c171126dc5d9c3100
Yaqcodes <shuaibkhaleefa@gmail.com>
2024-10-22 13:19:34 +0100

    expiry time typecast

--- mqtt_handler/views.py
+++ mqtt_handler/views.py
@@ -389,9 +389,9 @@ class DirectControlView(APIView):
         # Schedule sub_asset_expiry
         current_time = timezone.now()
         if settings.DEBUG:
-            expiry_time = current_time + timedelta(seconds=settings.DIRECT_CONTROL_EXPIRY)
+            expiry_time = current_time + timedelta(seconds=int(settings.DIRECT_CONTROL_EXPIRY))
         else:    
-            expiry_time = current_time + timedelta(hours=settings.DIRECT_CONTROL_EXPIRY)
+            expiry_time = current_time + timedelta(hours=int(settings.DIRECT_CONTROL_EXPIRY))
 
         expiry_data = None
         # Set expiry for hotel
