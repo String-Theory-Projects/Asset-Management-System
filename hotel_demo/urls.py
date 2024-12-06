324f10dde7ebc4b65a1117b6e1cee92d1da0b6ce
Bnabdulwasiu <abdulrahmanabdulwasiu111@gmail.com>
2024-12-06 21:57:14 +0100

    Collect staticfiles

--- hotel_demo/urls.py
+++ hotel_demo/urls.py
@@ -27,7 +27,7 @@ schema_view = get_schema_view(
    openapi.Info(
       title="TryKey API",
       default_version='v1',
-      description="Test description",
+      description="Asset description",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="info.trykey@gmail.com"),
       license=openapi.License(name="BSD License"),
